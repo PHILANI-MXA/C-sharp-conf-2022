5749c9839a754885de07da300cf569482c4b0a5a
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-11 14:01:52 +0200

    conference 2022

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// var person = new Person { FirstName = "Tracy", LastName = "Smith"};
+
+// [Person(FirstName ="Tracy", LastName ="Smith")]
+
+// public class Person: Attribute
+// {
+//     required public string FirstName { get; init; }
+//     public string? MiddleName { get; init; }
+//     required public string LastName { get; init; }
+// }
+
+var result = AddAll(new[] { 1, 2, 3, 4, 5});
+Console.WriteLine(result);
+
+// int AddAll(int[] values)
+// {
+//     int result = 0;
+//     foreach (var value in values)
+//     {
+//         result += value;
+//         return result;
+//     }
+// }
+
+T AddAll <T>(T[] values) where T : INumber<T>
+{
+    T result = T.Zero;
+    foreach (var value in values)
+    {
+        result += value;
+    }
+     return result;
+}
